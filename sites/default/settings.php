62eba7bfbb47d31f36943f801dd1b5e6913d91c7
Arty <26905074+artysidorenko@users.noreply.github.com>
2019-05-19 08:52:56 +0100

    revised db connection details to heroku clearDB

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -787,13 +787,15 @@ $settings['entity_update_backup'] = TRUE;
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 $databases['default']['default'] = array (
-  'database' => 'drupal',
-  'username' => 'drupal',
-  'password' => 'drupal',
+  'database' => 'heroku_15f7879c56b5929',
+  'username' => 'b967f457ca6436',
+  'password' => '3af29494',
   'prefix' => '',
-  'host' => 'localhost',
+  'host' => 'us-cdbr-iron-east-02.cleardb.net',
   'port' => '3306',
   'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
   'driver' => 'mysql',
 );
 $config_directories['sync'] = 'sites/default/files/config_7c9XaO56QNlWo125aCT7UOWNS3Nb7CLQvuJ_AIn91yJDN4VkvjYlSvjeI5fIiPoP6UNWdpGqMQ/sync';
+
+// heroku config variable: mysql: //b967f457ca6436:3af29494@us-cdbr-iron-east-02.cleardb.net/heroku_15f7879c56b5929?reconnect=true
